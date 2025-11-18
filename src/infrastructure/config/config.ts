06b31e623bd87ff48992b64226d763b2e1a3acf7
camilo ruiz <camiloruizvidal@gmail.com>
2025-11-18 09:36:25 -0500

    separando pdf de plantillas

--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -6,3 +6,17 @@ export class Config {
   static readonly puerto = Number(process.env.PORT);
   static readonly jwtKey = process.env.JWT_KEY;
 }
+
+const errors: string[] = [];
+Object.keys(Config).forEach((key) => {
+  if (
+    Config[key] === null ||
+    Config[key] === undefined ||
+    `${Config[key]}`.trim() === ''
+  ) {
+    errors.push(`La variable de entorno ${key} es requerida`);
+  }
+});
+if (errors.length > 0) {
+  throw new Error(errors.join('\n'));
+}
